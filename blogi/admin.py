9dce5629e1584373f9f81f596a883fa3daabcbf0
NikoKemppainen <48498@edu.raseko.fi>
2023-02-17 09:22:12 +0200

    joo jtn

--- blogi/admin.py
+++ blogi/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Postaus 
+
+@admin.register(Postaus)
+class PostausAdmin(admin.ModelAdmin):
+    list_display = ["otsikko", "luotu"]
--- blogi/views.py
+++ blogi/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
-# Create your views here.
+from .models import Postaus
+
+
+def postaukset(request):
+    postaukset = Postaus.objects.all()
+    context = {"postaukset": postaukset}
+    return render(
+        request,
+        "blogi/postauslista.html",
+        context,
+    )
+
+
+def nayta_postaus(request, id):
+    postaus = Postaus.objects.get(id=id)
+    context = {"postaus": postaus}
+    return render(request, "blogi/postaus.html", context)
+
+
+def uusi_postaus(request):
+    if request.method == "GET":
+        # 1. Lomake näytetään ensimmäistä kertaa
+        return render(request, "blogi/uusi postaus.html")
+    elif request.method == "POST":
+        # 2. Käyttäjä täytti ja lähetti lomakkeen
+
+        # Luetaan lomakkeen tiedot POST-datasta
+        otsikko = request.POST['otsikko']
+        teksti= request.POST['teksti']
+
+        # Luodan uusi Postaus-objekti
+        postaus = Postaus.objects.create(
+            otsikko=otsikko,
+            teksti=teksti,
+        )
+
+        # Muodostetaan URL-osoite luotuun Postaus-objektiin
+        url = reverse('nayta_postaus', args=(postaus.id,))
+        
+        # Palautetaan uudelleenohjaus uuden postaus-objektin URL:iin
+        return redirect (url)
