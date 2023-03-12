420d97f69ce073879fd7300e0a1f6dbe7cfc4944
NikoKemppainen <48498@edu.raseko.fi>
2023-03-12 18:30:19 +0200

    joo o

--- blogi/models.py
+++ blogi/models.py
@@ -4,7 +4,13 @@ from django.db import models
 class Postaus(models.Model):
     otsikko = models.CharField(max_length=200)
     teksti = models.TextField()
+    kuva = models.ImageField(null=true)
     luotu = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.otsikko
+    
+    def luo_ingressi(self):
+        if self.ingressi:
+            return self.ingressi
+        return self.tekstit[:50] + "..."
